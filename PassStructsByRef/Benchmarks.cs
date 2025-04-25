5590aa4387791604d35abad84e8e92133d218421
Simon Da Vall <simon.davall@gmail.com>
2025-04-25 12:40:32 +0100

    pass structs by ref using the 'in' keyword

---
+++ PassStructsByRef/Benchmarks.cs
@@ -0,0 +1,77 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace PassStructsByRef;
+
+[MemoryDiagnoser]
+public class Benchmarks {
+
+    [Benchmark]
+    public int UsingNormalStruct() {
+        var points = new MutablePoint[100];
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
+                points[i + j] = new MutablePoint(i, j);
+            }
+        }
+        var result = 0;
+        for (var i = 0; i < points.Length; ++i) {
+            result += GetNewX(points[i]);
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public int UsingReadonlyStruct() {
+        var points = new ReadOnlyPoint[100];
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
+                points[i + j] = new ReadOnlyPoint(i, j);
+            }
+        }
+        var result = 0;
+        for (var i = 0; i < points.Length; ++i) {
+            result += GetNewX(points[i]);
+        }
+        return result;
+    }
+    
+    [Benchmark]
+    public int UsingReadonlyStructWithIn() {
+        var points = new ReadOnlyPoint[100];
+        for (var i = 0; i < 10; i++) {
+            for (var j = 0; j < 10; j++) {
+                points[i + j] = new ReadOnlyPoint(i, j);
+            }
+        }
+        
+        var result = 0;
+        for (var i = 0; i < points.Length; ++i) {
+            result += GetNewXWithIn(points[i]);
+        }
+        return result;
+    }
+
+    private int GetNewX(MutablePoint point) {
+        return point.X + 1;
+    }
+    
+    private int GetNewX(ReadOnlyPoint point) {
+        return point.X + 1;
+    }
+    
+    private int GetNewXWithIn(in ReadOnlyPoint point) {
+        return point.X + 1;
+    }
+    
+    private struct MutablePoint (int x, int y) {
+        public int X = x;
+        public int Y = y;
+        public string Name = $"{x}, {y}";
+    }
+    
+    private readonly struct ReadOnlyPoint (int x, int y) {
+        public readonly int X = x;
+        public readonly int Y = y;
+        public readonly string Name = $"{x}, {y}";
+    }
+}
---
+++ PassStructsByRef/Readme.md
@@ -0,0 +1,14 @@
+﻿## Pass Structs By Ref ##
+
+Benchmarking to compare various different methods of passing structs and the effect on performance.
+
+Despite various attempts, I cannot seem to get a perf difference passing by ref and not. I think I
+must be doing something wrong.
+
+Will revisit this at some point.
+
+| Method                    | Mean     | Error     | StdDev    | Gen0   | Gen1   | Allocated |
+|-------------------------- |---------:|----------:|----------:|-------:|-------:|----------:|
+| UsingNormalStruct         | 3.047 us | 0.0509 us | 0.0425 us | 0.7668 | 0.0038 |   4.71 KB |
+| UsingReadonlyStruct       | 3.033 us | 0.0387 us | 0.0302 us | 0.7668 | 0.0038 |   4.71 KB |
+| UsingReadonlyStructWithIn | 3.025 us | 0.0351 us | 0.0311 us | 0.7668 | 0.0038 |   4.71 KB |
